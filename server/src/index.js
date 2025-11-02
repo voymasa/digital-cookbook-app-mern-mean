db4539bd87cddf7fd686c687dcfd89c35be64982
avoymas01@gmail.com <avoymas01@gmail.com>
2025-11-02 10:56:56 -0500

    Added recipe controller, model, routers, repo, and updated index.js to use recipe routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,14 @@
 // think of this file like Program.cs for a .Net backend
 import express from "express";
 import cors from "cors";
+import { RecipeRoutes } from "./routes/recipeRoutes";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/recipes", RecipeRoutes);
 
 app.listn(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
